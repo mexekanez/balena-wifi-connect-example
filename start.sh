c91ae2a8b2621626fefccbe38e517a2dc98ad674
Ivan <i.shevchenko@flavosil.com.ua>
2019-08-29 17:31:21 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Enable i2c - needed for the Display-O-Tron HAT
-modprobe i2c-dev
+#modprobe i2c-dev
 
 # Run one process loop
-python src/process.py
+#python src/process.py
 
 # Start resin-wifi-connect
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
